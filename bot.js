b849a0427637ee4700dfc5868976976606f91af7
hello-worldyj <sis00011086@gmail.com>
2025-12-31 11:56:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new Client({
   intents: [
@@ -8,39 +10,16 @@ const client = new Client({
   ]
 });
 
-const requests = global.requests; // server.js 와 공유
-
 client.once("ready", () => {
   console.log("🤖 !rate 봇 로그인 완료");
 });
 
 client.on("messageCreate", async (msg) => {
   if (msg.author.bot) return;
-  if (!msg.content.startsWith("!rate")) return;
 
-  const parts = msg.content.split(" ");
-  if (parts.length < 3) {
-    msg.reply("형식: !rate <ID> <평가>");
-    return;
+  if (msg.content.startsWith("!rate")) {
+    await msg.reply("사진은여??");
   }
-
-  const id = parts[1];
-  const result = parts.slice(2).join(" ");
-
-  if (!requests[id]) {
-    msg.reply("❌ 해당 ID 없음");
-    return;
-  }
-
-  if (requests[id].status === "done") {
-    msg.reply("❌ 이미 평가됨");
-    return;
-  }
-
-  requests[id].status = "done";
-  requests[id].result = result;
-
-  msg.reply(`✅ 평가 완료: **${result}**`);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
